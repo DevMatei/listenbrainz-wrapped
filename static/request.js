c311cb07a01aa0638000e05c120126dfca422dcb
parlajatwit <parlaj@wit.edu>
2022-12-05 11:22:42 -0500

    top genre could use threading to be a bit faster by making more requests? or less artists used to calculate

--- static/request.js
+++ static/request.js
@@ -2,6 +2,7 @@ var btn = null;
 var username_field = null;
 var top_artists = null;
 var top_songs = null;
+var top_genre = null;
 var listen_time = null;
 var artist_img = null;
 var color_dropdown = null;
@@ -20,25 +21,32 @@ async function generate_wrapped() {
     username = username_field.value;
     get_listen_time();
     get_artist_img();
-    get_top_artists();
-    get_top_songs();
+    get_top_artists(5);
+    get_top_songs(5);
+    get_top_genre();
     generated = true;
 }
 async function get_artist_img() {
     artist_img.src = await (await fetch("/top/img/" + username)).text();
     draw_artist_img();
 }
-async function get_top_artists() {
-    top_artists.innerHTML = await (await fetch("/top/artists/" + username)).text();
+async function get_top_artists(number) {
+    top_artists.innerHTML = await (await fetch("/top/artists/" + username + "/" + number + "/formatted")).text();
     draw_top_artists();
 }
-async function get_top_songs() {
-    top_songs.innerHTML = await (await fetch("/top/tracks/" + username)).text();
+async function get_top_songs(number) {
+    top_songs.innerHTML = await (await fetch("/top/tracks/" + username + "/" + number + "/formatted")).text();
     draw_top_songs();
 }
 async function get_listen_time() {
     listen_time.innerHTML = await (await fetch("/time/total/" + username)).text();
     draw_listen_time();
+}
+async function get_top_genre() {
+    top_genre.innerHTML = await (await fetch("/top/genre/user/" + username)).text();
+    if (top_genre.innerHTML != "no genre") {
+        draw_top_genre();
+    }
 }
 
 function draw_bg() {
@@ -67,7 +75,9 @@ function draw_bg() {
        draw_listen_time();
        draw_top_artists();
        draw_top_songs();
-       //draw_top_genres();
+       if (top_genre.innerHTML != "no genre") {
+        draw_top_genre();
+       }
     }
 }
 function draw_artist_img() {
@@ -104,12 +114,15 @@ function draw_listen_time() {
     ctx.font = "48px Nunito-Bold";
     ctx.fillText("Minutes Listened", 112, 1475);
     ctx.font = "77px Nunito-Bold";
-    ctx.fillText(listen_time.innerHTML.toLocaleString('en-US'), 112, 1575);
+    ctx.fillText(listen_time.innerHTML, 112, 1575);
 }
 
 // does nothing
 function draw_top_genre() {
+    ctx.font = "48px Nunito-Bold";
     ctx.fillText("Top Genre", 565, 1475);
+    ctx.font = "77px Nunito-Bold"; 
+    ctx.fillText(top_genre.innerHTML, 565, 1575);
 }
 
 window.onload = function() {
@@ -118,6 +131,7 @@ window.onload = function() {
     username_field = document.getElementById('username');
     top_artists = document.getElementById('top-artists');
     top_songs = document.getElementById('top-tracks');
+    top_genre = document.getElementById('top-genre');
     listen_time = document.getElementsByTagName('listen-time');
     artist_img = document.getElementById('artist-img');
     color_dropdown = document.getElementById('color');
--- wrapped-fm.py
+++ wrapped-fm.py
@@ -5,6 +5,7 @@ import subprocess
 import time
 from flask import Flask
 from bs4 import BeautifulSoup
+from statistics import mode
 
 api_file = open(os.getcwd() + "/key", "r")
 key = api_file.readline()
@@ -17,9 +18,9 @@ def root():
     return app.send_static_file('index.html')
 
 # returns the top albums given a username
-@app.route("/top/albums/<name>")
-def get_top_albums(name):
-    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': '5', 'format': 'json'}
+@app.route("/top/albums/<name>/<number>")
+def get_top_albums(name, number):
+    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': 'number', 'format': 'json'}
     r = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.gettopalbums', params=payload)
     output = ''
     for i in r.json()['topalbums']['album']:
@@ -27,20 +28,30 @@ def get_top_albums(name):
     return output
 
 # returns the top artists given a username
-@app.route("/top/artists/<name>")
-def get_top_artists(name):
-    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': '5', 'format': 'json'}
+@app.route("/top/artists/<name>/<number>")
+def get_top_artists(name, number):
+    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': number, 'format': 'json'}
     r = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.gettopartists', params=payload)
 
-    output = ""
+    output = []
     for i in r.json()['topartists']['artist']:
-       output += f"{i['@attr']['rank']} {i['name']}<br>"
+       output.append(i['name'])
     return output
+
+# returns the top artists formatted in 1 name\n 2 name\n ....
+@app.route("/top/artists/<name>/<number>/formatted")
+def get_top_artists_formatted(name, number):
+    artists = get_top_artists(name, number)
+    output = ""
+    for n in range(0, int(number)):
+        output += f"{str(n+1)} {artists[n]}<br>"
+    return output
+
 
 # returns the top artist url
 @app.route("/top/img/<name>")
 def get_top_artist_img(name):
-    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': '5', 'format': 'json'}
+    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': '1', 'format': 'json'}
     r = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.gettopartists', params=payload)
 
     topname = r.json()['topartists']['artist'][0]['name']
@@ -51,14 +62,24 @@ def get_top_artist_img(name):
     return img_url
 
 # returns the top tracks given a username
-@app.route("/top/tracks/<name>")
-def get_top_tracks(name):
-    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': '5', 'format': 'json'}
+@app.route("/top/tracks/<name>/<number>")
+def get_top_tracks(name, number):
+    payload = {'user': name, 'api_key': key, 'period': '12month', 'limit': number, 'format': 'json'}
     r = requests.get('https://ws.audioscrobbler.com/2.0/?method=user.gettoptracks', params=payload)
-    output = ''
+    output = []
     for i in r.json()['toptracks']['track']:
-       output += f"{i['@attr']['rank']} {i['name']}<br>"
+       output.append(i['name'])
     return output
+
+# returns the top tracks formatted in 1 name\n 2 name\n ....
+@app.route("/top/tracks/<name>/<number>/formatted")
+def get_top_tracks_formatted(name, number):
+    tracks = get_top_tracks(name, number)
+    output = ""
+    for n in range(0, int(number)):
+        output += f"{str(n+1)} {tracks[n]}<br>"
+    return output
+
 
 # returns the total listening time given a username
 @app.route("/time/total/<name>")
@@ -69,12 +90,13 @@ def get_listen_time(name):
     r = requests.get('http://ws.audioscrobbler.com/2.0/?method=user.getweeklytrackchart', params=payload)
     total = 0
     for i in r.json()['weeklytrackchart']['track']:
+        print("fuck")
         playcount = int(i['playcount'])
         tracktime = get_track_time(i['artist']['#text'], i['name']) 
         total += playcount * int(get_track_time(i['artist']['#text'], i['name']))
     return ("{:,}".format(int(total/60000)))
 
-# returns the length for a track given an mbid
+# returns the length for a track given an artist and track name
 @app.route("/time/total_ta/<artist>/<track>")
 def get_track_time(artist, track):
     payload = {'artist': artist, 'track': track, 'api_key': key, 'format': 'json'}
@@ -85,6 +107,30 @@ def get_track_time(artist, track):
         output = "180000" # 3 minutes is roughly the average song length, good enough if it doesn't find a track duration
     return output
 
+# returns the top genre for this user
+@app.route("/top/genre/user/<name>")
+def get_top_genre_user(name):
+    genres = []
+    artists = get_top_artists(name, 15)
+    for artist in artists:
+        genres.append(get_top_genre_artist(artist))
+    return mode(genres)
+    
+
+# returns the top tag (genre) for an artist
+@app.route("/top/genre/artist/<artist>")
+def get_top_genre_artist(artist):
+    payload = {'artist': artist, 'api_key': key, 'format': 'json'}
+    r = requests.get('http://ws.audioscrobbler.com/2.0/?method=artist.gettoptags', params=payload)
+    try:
+        output = r.json()['toptags']['tag'][0]['name']
+        index = 0
+        while output == "seen live": # have a list of prohibited tags? mainstream genre list to check against? idk
+            index += 1
+            output = r.json()['toptags']['tag'][index]['name']
+    except:
+        output = "no genre"
+    return output
 
 if __name__ == "__main__":
     app.run()
